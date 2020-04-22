67080b0f8d3be608686503c7c91656800e3756e3
Omid2121 <Omid_Ahangi@github.com>
2020-04-22 14:55:54 -0700

    dag 1 på space invaders

---
+++ spaceInvaders/MYspaceInvaders/Assets/Scripts/AlienBullet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienBullet : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    public float speed = 30;
+    public Sprite explodedShipImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        rigidBody.velocity = Vector2.down * speed;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+
+        if (col.gameObject.tag == "Player")
+        {
+            col.GetComponent<SpriteRenderer>().sprite = explodedShipImage;
+
+            Destroy(gameObject);
+            DestroyObject(col.gameObject, 0.5f);
+        }
+
+        if (col.tag == "Shield")
+        {
+            Destroy(gameObject);
+            DestroyObject(col.gameObject);
+        }
+
+    }
+
+     void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ spaceInvaders/MYspaceInvaders/Assets/Scripts/Bullet.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 30;
+
+    private Rigidbody2D rigidBody;
+    public Sprite explodedAlienImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        rigidBody.velocity = Vector2.up * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+
+        if (col.tag =="Alien")
+        {
+            IncreaseTextUIScore();
+
+            col.GetComponent<SpriteRenderer>().sprite = explodedAlienImage;
+            DestroyObject(col.gameObject, 0.5f);
+
+            Destroy(gameObject);
+
+           
+        }
+
+        if (col.tag == "Shield")
+        {
+            Destroy(gameObject);
+            DestroyObject(col.gameObject);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+    void IncreaseTextUIScore()
+    {
+        var textUIComp = GameObject.Find("Score").GetComponent<Text>();
+
+        int score = int.Parse(textUIComp.text);
+
+        score += 10;
+
+        textUIComp.text = score.ToString();
+
+    }
+}
